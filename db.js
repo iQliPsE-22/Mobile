a5c13967906070bdb2e8f35dae88cf99b0045f07
Deepak Kumar <deepaklg02@gmail.com>
2024-01-10 16:48:46 +0530

    PUSHING

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 async function connectToDatabase() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/user", {
+  await mongoose.connect("mongodb://127.0.0.1:27017/", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
